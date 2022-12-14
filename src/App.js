c67532313b559d6402c6486321d8f375d7500463
harshika-12 <chennareddyharshika99@gmail.com>
2022-12-14 15:48:12 +0530

    intial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>test reactcd my-app</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          hres="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
